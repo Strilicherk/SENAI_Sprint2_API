4a2ca23f1ae20e5e142c871ef35393458c62673c
MatheusStrilicherkDEV <matheuscherk@hotmail.com>
2021-04-16 09:18:46 -0300

    Método Delete, BuscarPorId, Atualizar, SQL Injection

--- senai_filmes_webApi/senai_filmes_webApi/Controllers/GenerosController.cs
+++ senai_filmes_webApi/senai_filmes_webApi/Controllers/GenerosController.cs
@@ -41,6 +41,7 @@ namespace senai_filmes_webApi.Controllers
         /// Lista todos os generos
         /// </summary>
         /// <returns>Uma lista de gêneros e um status code</returns>
+        /// http://localhost:5000/api/generos
         [HttpGet]
         public IActionResult Get()
         {
@@ -49,6 +50,145 @@ namespace senai_filmes_webApi.Controllers
 
             // Retorna o status code 200 (Ok) com a lista de gêneros no formato JSON
             return Ok(listaGeneros);
+        }
+
+        /// <summary>
+        /// Busca um gênero através do seu id
+        /// </summary>
+        /// <param name="id">id do gênero que será buscado</param>
+        /// <returns>Um gênero buscado ou NotFound caso nenhum gênero seja encontrado</returns>
+        /// http://localhost:5000/api/generos/id
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {   
+            // Cria um objeto generoBuscado que irá receber o gênero buscado
+            GeneroDomain generoBuscado = _generoRepository.BuscarPorId(id);
+
+            if (generoBuscado == null)
+            {   
+                // Retorna um NotFound se o gênero buscado for igual a null
+                return NotFound("Nenhum gênero encontrado");
+            }
+
+            // Retorna o status code 200 (Ok) com o gênero buscado caso haja algum
+            return Ok(generoBuscado);
+        }
+
+        /// <summary>
+        /// Cadastra um novo gênero
+        /// </summary>
+        /// <returns>Um status code 201 - Created</returns>
+        /// http://localhost:5000/api/generos
+        [HttpPost]
+        public IActionResult Post(GeneroDomain novoGenero)
+        {
+            // Faz a chamada para o método .Cadastrar
+            _generoRepository.Cadastrar(novoGenero);
+
+            // Retorna um status code 201 - Created
+            return StatusCode(201);
+        }
+
+        /// <summary>
+        /// Atualiza um gênero existente passando seu id pela url da requisição
+        /// </summary>
+        /// <param name="id">id do gênero que será atualizado</param>
+        /// <param name="generoAtualizado">Objeto generoAtualizado com as novas informações</param>
+        /// <returns>Um status code</returns>
+        /// http://localhost:5000/api/generos/id
+        [HttpPut("{id}")]
+        public IActionResult PutIdUrl(int id, GeneroDomain generoAtualizado)
+        {
+            // Cria um objeto generoBuscado que irá receber o gênero buscado
+            GeneroDomain generoBuscado = _generoRepository.BuscarPorId(id);
+
+            if (generoBuscado == null)
+            {
+                // Retorna um NotFound se o gênero buscado for igual a null
+                return NotFound
+                    (
+                        new
+                        {
+                            mensagem = "gênero não encontrado",
+                            erro = true
+                        }
+                    );
+            }
+
+            // Tenta atualizar o registro
+            try
+            {
+                // Faz a chamada para o método .AtualizarIdUrl()
+                _generoRepository.AtualizarIdUrl(id, generoAtualizado);
+
+                // Retorna um status code 204 - No Content
+                return NoContent();
+            }
+
+            // Caso ocorra algum erro
+            catch (Exception codErro)
+            {
+                // Retorna um status code 400 - BadRequest e o código do erro
+                return BadRequest(codErro);
+            }
+        }
+
+        /// <summary>
+        /// Atualiza um gênero existente passando seu id pelo corpo da requisição
+        /// </summary>
+        /// <param name="id">objeto generoAtualizado com as novas informações</param>
+        /// <returns>Um status code</returns>
+        [HttpPut]
+        public IActionResult PutIdBody(GeneroDomain generoAtualizado)
+        {
+            // Cria um objeto generoBuscado que irá receber o gênero buscado
+            GeneroDomain generoBuscado = _generoRepository.BuscarPorId(generoAtualizado.idGenero);
+
+            // Verifica se algum gênero foi encontrado
+            if (generoBuscado != null)
+            {
+
+                // Tenta atualizar o registro
+                try
+                {
+                    // Faz a chamada para o método .AtualizarIdCorpo()
+                    _generoRepository.AtualizarIdCorpo(generoAtualizado);
+
+                    // Retorna um status code 204 - No Content
+                    return NoContent();
+                }
+                // Caso ocorra algum erro
+                catch (Exception codErro)
+                {
+                    // Retorna BadResquest com o código do erro
+                    return BadRequest(codErro);
+
+                }
+            }
+                // Caso não seja encontrado retorna NotFoundo com mensagem personalizada
+                return NotFound
+                    (
+                        new
+                        {
+                            mensagem = "gênero não encontrado"
+                        }
+                    );
+        }
+
+        /// <summary>
+        /// Deleta um gênero existente
+        /// </summary>
+        /// <param name="id">id do gênero que será deletado</param>
+        /// <returns>Um status code 204 - No Content</returns>
+        /// http://localhost:5000/api/generos/id
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            // Chama o método .Deletar()
+            _generoRepository.Deletar(id);
+
+            // Retorna um status code 204 - No Content
+            return StatusCode(204);
         }
     }
 
--- senai_filmes_webApi/senai_filmes_webApi/Repositories/GeneroRepository.cs
+++ senai_filmes_webApi/senai_filmes_webApi/Repositories/GeneroRepository.cs
@@ -19,29 +19,169 @@ namespace senai_filmes_webApi.Repositories
         /// </summary>
         private string stringConexao = "Data Source=DESKTOP-Q0JOIHU\\SQLEXPRESS; initial catalog=Filmes; user Id=sa; pwd=strilicherk2012n";
 
+        /// <summary>
+        /// Atualiza um gênero passando o id pelo corpo da requisição
+        /// </summary>
+        /// <param name="genero">Objeto gênero com as novas informações</param>
         public void AtualizarIdCorpo(GeneroDomain genero)
         {
-            throw new NotImplementedException();
+            // Declara a SqlConnection con passando a string de conexão como parâmetro
+            using (SqlConnection con = new SqlConnection(stringConexao))
+            {
+                // Declara a query a ser executada
+                string queryUpdateIdBody = "UPDATE Generos SET Nome = @Nome WHERE idGenero = @ID";
+
+                // Declara o SqlCommand cmd passando a query que será executada e a conexão como parâmetros
+                using (SqlCommand cmd = new SqlCommand(queryUpdateIdBody, con))
+                {
+                    // Passa os valores para os parâmetros
+                    cmd.Parameters.AddWithValue("@ID", genero.idGenero);
+                    cmd.Parameters.AddWithValue("@Nome", genero.nome);
+
+                    // Abre a conexão com o banco
+                    con.Open();
+
+                    // Executa o comando
+                    cmd.ExecuteNonQuery();
+                }
+            }
         }
 
+        /// <summary>
+        /// Atualiza um gênero passando o id pela URL
+        /// </summary>
+        /// <param name="id">id do gênero que será atualizado</param>
+        /// <param name="genero">Objeto gênero com as novas informações</param>
         public void AtualizarIdUrl(int id, GeneroDomain genero)
         {
-            throw new NotImplementedException();
-        }
+            // Declara a SqlConnection con passando a string de conexão como parâmetro
+            using (SqlConnection con = new SqlConnection(stringConexao))
+            {
+                // Declara a query a ser executada
+                string queryUpdateIdUrl = "UPDATE Generos SET Nome = @Nome WHERE idGenero = @ID";
 
+                // Declara o SqlCommand cmd passando a query que será executada e a conexão como parâmetros
+                using (SqlCommand cmd = new SqlCommand(queryUpdateIdUrl, con))
+                {
+                    // Passa os valores para os parâmetros
+                    cmd.Parameters.AddWithValue("@ID", id);
+                    cmd.Parameters.AddWithValue("@Nome", genero.nome);
+
+                    // Abre a conexão com o banco
+                    con.Open();
+
+                    // Executa o comando
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+        /// <summary>
+        /// Busca um gênero pelo id
+        /// </summary>
+        /// <param name="id">id do gênero que será buscado</param>
+        /// <returns>Um gênero buscado ou null</returns>
         public GeneroDomain BuscarPorId(int id)
         {
-            throw new NotImplementedException();
+            // Declara a SqlConnection con passando a string de conexão como parâmetro
+            using (SqlConnection con = new SqlConnection(stringConexao))
+            {
+                // Declara a query a ser executada
+                string querySelectByID = "SELECT idGenero, Nome FROM Generos WHERE idGenero = @ID";
+                // Abre a conexão com o banco de dados
+                con.Open();
+                // Declara o SqlDataReader rdr para receber o valor do banco de dados
+                SqlDataReader rdr;
+                // Declara o SqlCommand cmd passando a query que será executada e a conexão como parâmetros
+                using (SqlCommand cmd = new SqlCommand(querySelectByID, con))
+                {
+                    // Passa o valor para o parâmetro ID
+                    cmd.Parameters.AddWithValue("@ID", id);
+
+                    // Executa a query e armazena os dados no rdr
+                    rdr = cmd.ExecuteReader();
+
+                    // Verifica se o resultado da Query retornou algum valor
+                    if (rdr.Read())
+                    {
+                        // Se sim, instaciamos um novo objeto chamado generoBuscado do tipo GeneroDomain
+                        GeneroDomain generoBuscado = new GeneroDomain
+                        {
+                            //Atribui à propriedade idGenero o valor da primeira coluna da tabela do banco de dados
+                            idGenero = Convert.ToInt32(rdr["idGenero"]),
+
+                            //Atribui à propriedade idGenero o valor da segunda coluna da tabela do banco de dados
+                            nome = rdr["Nome"].ToString()
+                        };
+
+                        // Retorna o gênero buscado
+                        return generoBuscado;
+                    }
+
+                    // else retur null
+                    return null;
+                }
+            }
         }
 
+        /// <summary>
+        /// Cadastra um novo gênero
+        /// </summary>
+        /// <param name="novoGenero">Objeto novoGenero com as informações que serão cadastradas</param>
         public void Cadastrar(GeneroDomain novoGenero)
         {
-            throw new NotImplementedException();
+            // Declara a SqlConnection con passando a string de conexão como parâmetro
+            using (SqlConnection con = new SqlConnection(stringConexao))
+            {
+                // Declara a instrução a ser executada
+                // string queryInsert = "INSERT INTO Generos(Nome) VALUES ('" + novoGenero.nome + "')";
+                // Não usar dessa forma pois permite SQL Injection e causa o efeito Joana D'Arc
+                // Por exemplo:
+                // "nome" : "Perdeu mané') DROP TABLE Filmes--"
+
+                //Declara a instrução a ser executada
+                string queryInsert = "INSERT INTO Generos(Nome) VALUES (@Nome)";
+
+                // Declara o SqlCommand cmd passando a query que será executada e a conexão como parâmetros
+                using (SqlCommand cmd = new SqlCommand(queryInsert, con))
+                {
+                    // Passa o valo do parâmetro @Nome
+                    cmd.Parameters.AddWithValue("@Nome", novoGenero.nome);
+
+                    // Abre a conexão com o banco de dados
+                    con.Open();
+
+                    //Executa a query
+                    cmd.ExecuteNonQuery();
+                }
+            }
         }
 
+        /// <summary>
+        /// Deleta um gênero através do seu id
+        /// </summary>
+        /// <param name="id">id do gênero que será deletado</param>
         public void Deletar(int id)
         {
-            throw new NotImplementedException();
+            // Declara a SqlConnection con passando a string de conexão como parâmetro
+            using (SqlConnection con = new SqlConnection(stringConexao))
+            {
+                //Declara a query a ser executada passando o parâmetro @ID
+                string queryDelete = "DELETE FROM Generos WHERE idGenero = @ID";
+
+                // Declara o SqlCommand cmd passando a query que será executada e a conexão como parâmetros
+                using (SqlCommand cmd = new SqlCommand(queryDelete, con))
+                {
+                    // Passa o valo do parâmetro @Nome
+                    cmd.Parameters.AddWithValue("@ID", id);
+
+                    // Abre a conexão com o banco de dados
+                    con.Open();
+
+                    //Executa a query
+                    cmd.ExecuteNonQuery();
+                }
+
+            }
         }
 
         public List<GeneroDomain> ListarTodos()
@@ -76,6 +216,9 @@ namespace senai_filmes_webApi.Repositories
                             //Atribui à propriedade idGenero o valor da segunda coluna da tabela do banco de dados
                             nome = rdr[1].ToString()
                         };
+                        
+                        // Adiciona o objeto genero criado à lista
+                        listaGeneros.Add(genero);
                     }
                 }
             }
